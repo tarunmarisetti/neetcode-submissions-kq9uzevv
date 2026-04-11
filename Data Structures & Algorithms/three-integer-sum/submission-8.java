c2b6d5e6ec0c4d666f1377e2195d8d0e7afe8273
Tarun Sai Marisetti <45293159+tarunmarisetti@users.noreply.github.com>
2026-04-11 13:37:26 -0700

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0; i< nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+               
+                int totalSum=nums[i]+nums[j]+nums[k];
+                if(totalSum==0){
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j+=1;
+                    k-=1;
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k+1]){
+                        k--;
+                    }
+
+                }
+                else if(totalSum>0){
+                    k-=1;
+                }
+                else{
+                    j+=1;
+                }
+            }
+        }
+        return res;
+    }
+}
