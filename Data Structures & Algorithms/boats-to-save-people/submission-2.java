2030e63bcfa4ad257c1ea07844e188d6c9119e39
Tarun Sai Marisetti <45293159+tarunmarisetti@users.noreply.github.com>
2026-04-11 13:57:53 -0700

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int l=0;
+        int r=people.length-1;
+        int boats=0;
+        while(l<=r){
+            if(people[l]+people[r]<=limit){
+                l+=1;
+            }
+            r-=1;
+            boats+=1;
+        }
+        return boats;
+        
+    }
+}
