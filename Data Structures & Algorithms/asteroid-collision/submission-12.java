812a40022a0c8ca5d52fae65f22d88d82634eda4
Tarun Sai Marisetti <45293159+tarunmarisetti@users.noreply.github.com>
2026-04-13 13:30:45 -0700

    Add: asteroid-collision - submission-12

---
+++ Data Structures & Algorithms/asteroid-collision/submission-12.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack=new ArrayDeque<>();
+        for(int asteroid: asteroids){
+            boolean isAlive=true;
+            while(!stack.isEmpty() && stack.peek()>0 && asteroid<0){
+                int top=stack.peek();
+                if(top<Math.abs(asteroid)){
+                    stack.pop();
+                }
+                else if(top>Math.abs(asteroid)){
+                    isAlive=false;
+                    break;
+                }
+                else if(top==Math.abs(asteroid)){
+                    stack.pop();
+                    isAlive=false;
+                    break;
+                }
+            }
+            if(isAlive){
+                stack.push(asteroid);
+            }
+        }
+        int n=stack.size();
+        int[] res=new int[n];
+        int index=n-1;
+        while(!stack.isEmpty()){
+            res[index--]=stack.pop();
+        }
+        return res;
+    }
+}
